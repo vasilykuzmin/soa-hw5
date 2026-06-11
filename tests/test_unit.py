a06ae8c6b7ac819815b7664617007bac747dcccf
Vasiliy Kuzmin <vasilykuzmin22@gmail.com>
2026-06-11 07:46:20 +0000

    fix

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -4,7 +4,8 @@ from datetime import datetime, timezone
 from unittest.mock import AsyncMock, Mock, patch
 
 import pytest
-
+import os
+import sys
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, parent_dir)
